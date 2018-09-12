b016da1b34f9c40192038fedafe3509f230b4dbc
root <root@localhost.localdomain>
2018-09-12 13:29:56 -0700

    update

--- test/test_common/test_time.cc
+++
@@ -1,9 +0,0 @@
-#include "test/test_common/test_time.h"
-
-#include "common/common/utility.h"
-
-namespace Envoy {
-
-DangerousDeprecatedTestTime::DangerousDeprecatedTestTime() {}
-
-} // namespace Envoy
