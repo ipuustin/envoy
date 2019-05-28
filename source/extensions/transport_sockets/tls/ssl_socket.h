d963550a8c218b4eea49edac7a72d717987c7063
Ismo Puustinen <ismo.puustinen@intel.com>
2019-05-28 14:48:45 +0300

    ssl_socket: check the private key method callback thread id.
    Signed-off-by: Ismo Puustinen <ismo.puustinen@intel.com>

--- source/extensions/transport_sockets/tls/ssl_socket.h
+++ source/extensions/transport_sockets/tls/ssl_socket.h
@@ -94,6 +94,7 @@ private:
   Network::PostIoAction doHandshake();
   void drainErrorQueue();
   void shutdownSsl();
+  bool isThreadSafe() const { return run_tid_->isCurrentThreadId(); }
 
   Network::TransportSocketCallbacks* callbacks_{};
   ContextImplSharedPtr ctx_;
@@ -107,6 +108,7 @@ private:
   mutable std::string cached_url_encoded_pem_encoded_peer_cert_chain_;
   std::vector<Envoy::Ssl::PrivateKeyConnectionPtr> pk_connections_;
   bool async_handshake_in_progress_{};
+  Thread::ThreadIdPtr run_tid_;
 };
 
 class ClientSslSocketFactory : public Network::TransportSocketFactory,
