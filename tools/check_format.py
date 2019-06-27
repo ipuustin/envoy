244f178ab1eab171658da35a1e78edcbf19f45ab
Ismo Puustinen <ismo.puustinen@intel.com>
2019-06-27 13:50:10 +0300

    tools: add qat to UNOWNED_EXTENSIONS.
    Signed-off-by: Ismo Puustinen <ismo.puustinen@intel.com>

--- tools/check_format.py
+++ tools/check_format.py
@@ -154,6 +154,7 @@ UNOWNED_EXTENSIONS = {
   "extensions/filters/network/mongo_proxy",
   "extensions/filters/network/common",
   "extensions/filters/network/common/redis",
+  "extensions/private_key_operations_providers/qat",
 }
 # yapf: enable
 
